8137a3e07a1b5fd72cb6f3ebc6468df836cbafd7
joveryn092101 <pagunsanjoveryn@gmail.com>
2024-12-03 12:30:28 +0800

    Add: variant in header for password

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,31 +24,105 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { Check, ChevronsUpDown } from "lucide-react";
 import { useTheme } from "next-themes";
+import Image from "next/image";
 import XONLogo from "data-base64:/assets/tokens/xon.png";
 
 const networks = [
   { value: "Mainnet", label: "Mainnet" },
-  { value: "Testnet", label: "Testnet" },
 ];
 
 interface HeaderProps {
-  currentPage: string;
-  onSetCurrentPage: (page: string) => void;
+  currentPage?: string;
+  onSetCurrentPage?: (page: string) => void;
+  variant?: "default" | "create-password"; 
 }
 
-const Header: React.FC<HeaderProps> = ({ currentPage, onSetCurrentPage }) => {
+const Header: React.FC<HeaderProps> = ({
+  currentPage,
+  onSetCurrentPage,
+  variant = "default",
+}) => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
   const { theme } = useTheme();
+
+  if (variant === "create-password") {
+    return (
+      <header 
+        className="flex justify-between items-center h-16 px-4 " 
+        style={{
+            background: "#2E266D",
+        }}
+      >
+        <h1 className="text-base font-semibold">Setup Password</h1>
+        <div className="w-[200px]">
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="roundedOutline"
+                role="combobox"
+                aria-expanded={open}
+                className="w-full float-right"
+                size="lg"
+              >
+                {value && (
+                  <div className="w-5 h-5 mr-2 relative">
+                    <Image
+                      src={XONLogo}
+                      alt="XON Logo"
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </div>
+                )}
+                {value
+                  ? networks.find((network) => network.value === value)?.label
+                  : "Select network"}
+                <ChevronsUpDown className="opacity-50 ml-2" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[240px] p-0">
+              <Command>
+                <CommandInput placeholder="Search network..." className="h-9" />
+                <CommandList>
+                  <CommandEmpty>No network found.</CommandEmpty>
+                  <CommandGroup>
+                    {networks.map((network) => (
+                      <CommandItem
+                        key={network.value}
+                        value={network.value}
+                        onSelect={(currentValue) => {
+                          setValue(currentValue === value ? "" : currentValue);
+                          setOpen(false);
+                        }}
+                      >
+                        {network.label}
+                        <Check
+                          className={cn(
+                            "ml-auto",
+                            value === network.value
+                              ? "opacity-100"
+                              : "opacity-0"
+                          )}
+                        />
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </header>
+    );
+  }
 
   return (
     <header className="flex top-0 h-16 shrink-0 items-center gap-2 px-4">
       <SidebarTrigger className="p-2" />
       <Separator
         orientation="vertical"
-        className={`mr-2 h-4 ${
-          theme === "light" ? "" : "bg-white"
-        }`}
+        className={`mr-2 h-4 ${theme === "light" ? "" : "bg-white"}`}
       />
       <Breadcrumb className="w-full">
         <BreadcrumbList>
@@ -69,11 +143,20 @@ const Header: React.FC<HeaderProps> = ({ currentPage, onSetCurrentPage }) => {
               className="w-full float-right"
               size="lg"
             >
-              <img src={XONLogo} className="w-2/12" alt="XON Logo" />{" "}
+              {value && (
+                <div className="w-5 h-5 mr-2 relative">
+                  <Image
+                    src={XONLogo}
+                    alt="XON Logo"
+                    layout="fill"
+                    objectFit="contain"
+                  />
+                </div>
+              )}
               {value
                 ? networks.find((network) => network.value === value)?.label
                 : "Select network"}
-              <ChevronsUpDown className="opacity-50" />
+              <ChevronsUpDown className="opacity-50 ml-2" />
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-[240px] p-0">
