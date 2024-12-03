765db711b5b44f5bcb82b2da1992524a8529acb4
joveryn092101 <pagunsanjoveryn@gmail.com>
2024-12-03 14:49:46 +0800

    Edit: label and input component ui

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "flex-1 px-3 py-2 w-full bg-[#1B2232] text-[#657AA2] placeholder:text-[#657AA2] focus:outline-none",
           className
         )}
         ref={ref}
