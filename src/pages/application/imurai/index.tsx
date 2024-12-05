27e558f1b83c1dfaa75642a5287883e4bda2fd39
JM Mac <johnmarkmancol@gmail.com>
2024-12-05 22:29:15 +0800

    feat: imurai page

--- src/pages/application/imurai/index.tsx
+++ src/pages/application/imurai/index.tsx
@@ -1,7 +1,17 @@
 import React from "react"
 
 const IndexImUrAi = () => {
-  return <div>IndexImUrAi</div>
+  return (
+    <div className="py-4">
+      <div className="bg-white p-2 rounded-lg flex gap-2">
+        <input
+          type="text"
+          placeholder="Type your message here..."
+          className="w-full bg-white text-black outline-none"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default IndexImUrAi
