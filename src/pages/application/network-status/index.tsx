24b56b0c53e925cdab433cb7d4483b249a0b82ed
butchcanz <butch.canada1994@gmail.com>
2024-12-05 14:21:12 +0800

    network status page

--- src/pages/application/network-status/index.tsx
+++ src/pages/application/network-status/index.tsx
@@ -1,86 +1,119 @@
-import { Banknote, Blocks, Hourglass, Wallet } from "lucide-react"
+import { Label } from "@/components/ui/label"
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table"
+import Addresses from "data-base64:/assets/app-logo/Address.png"
+import Historical from "data-base64:/assets/app-logo/Historical.png"
+import NFTCollection from "data-base64:/assets/app-logo/NFTCollection.png"
+import Pay from "data-base64:/assets/app-logo/Pay.png"
+// import { Banknote, Blocks, Hourglass, Wallet } from "lucide-react"
 import { useTheme } from "next-themes"
+import Image from "next/image"
 import React, { useEffect, useState } from "react"
+
+const formatNumber = (num) => {
+  if (num >= 1_000_000) {
+    return `${(num / 1_000_000).toFixed(1)}M`
+  }
+  return num.toLocaleString()
+}
 
 const IndexNetworkStatus = () => {
   const { theme } = useTheme()
+  const [networkStatus, setNetworkStatus] = useState({
+    totalBlocks: 0,
+    totalAddresses: 0,
+    avgBlockInterval: "0 secs",
+    lastGasFee: 0
+  })
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = {
+        totalBlocks: 1_125_915,
+        totalAddresses: 528,
+        avgBlockInterval: "12 s",
+        lastGasFee: 0.002
+      }
+      setNetworkStatus(data)
+    }
+
+    fetchData()
+  }, [])
   return (
     <>
-      <div className="grid grid-cols-2 gap-4">
-        <div>
-          <div
-            className={
-              theme == "light"
-                ? "aspect-video w-full rounded-lg bg-muted/50 text-center"
-                : "aspect-video w-full rounded-lg bg-[#405887] text-center text-white"
-            }>
-            <div className="flex items-center justify-center pt-4">
-              <Blocks size="70" />
-            </div>
-            <div className="mt-4">
-              <span className="text-base">Total Blocks</span>
-              <br />
-              <span className="text-lg font-bold">1,028,123</span>
-            </div>
-            <br />
-          </div>
-        </div>
-        <div>
-          <div
-            className={
-              theme == "light"
-                ? "aspect-video w-full rounded-lg bg-muted/50 text-center"
-                : "aspect-video w-full rounded-lg bg-[#405887] text-center text-white"
-            }>
-            <div className="flex items-center justify-center pt-4">
-              <Wallet size="70" />
-            </div>
-            <div className="mt-4">
-              <span className="text-base">Total Addresses</span>
-              <br />
-              <span className="text-lg font-bold">1,028,123</span>
-            </div>
-            <br />
-          </div>
-        </div>
-        <div>
-          <div
-            className={
-              theme == "light"
-                ? "aspect-video w-full rounded-lg bg-muted/50 text-center"
-                : "aspect-video w-full rounded-lg bg-[#405887] text-center text-white"
-            }>
-            <div className="flex items-center justify-center pt-4">
-              <Hourglass size="70" />
-            </div>
-            <div className="mt-4">
-              <span className="text-base">VG Block Intervals</span>
-              <br />
-              <span className="text-lg font-bold">1,028,123</span>
-            </div>
-            <br />
-          </div>
-        </div>
-        <div>
-          <div
-            className={
-              theme == "light"
-                ? "aspect-video w-full rounded-lg bg-muted/50 text-center"
-                : "aspect-video w-full rounded-lg bg-[#405887] text-center text-white"
-            }>
-            <div className="flex items-center justify-center pt-4">
-              <Banknote size="70" />
-            </div>
-            <div className="mt-4">
-              <span className="text-base">Last Gas Fee</span>
-              <br />
-              <span className="text-lg font-bold">1,028,123</span>
-            </div>
-            <br />
-          </div>
-        </div>
-      </div>
+      <Table className="mt-0">
+        <TableBody>
+          <TableRow className="flex flex-wrap justify-center">
+            <TableCell className="w-40 h-40 relative">
+              <div className="flex items-center justify-center bg-tablecell-detail rounded-xl h-full w-full">
+                <div className="text-center p-10 w-full">
+                  <Image
+                    src={NFTCollection}
+                    alt="NFTCollection"
+                    className="absolute top-0 left-0 ml-6 mt-6"
+                    width={35}
+                    height={35}
+                  />
+                  <p className="text-2xl font-extrabold text-purple mt-6">
+                    {formatNumber(networkStatus.totalBlocks)}
+                  </p>
+                  <Label className="text-sm">Total Blocks</Label>
+                </div>
+              </div>
+            </TableCell>
+            <TableCell className="w-40 h-40 relative">
+              <div className="flex items-center justify-center bg-tablecell-detail rounded-xl h-full w-full">
+                <div className="text-center p-10 w-full">
+                  <Image
+                    src={Addresses}
+                    alt="Addresses"
+                    className="absolute top-0 left-0 ml-6 mt-6"
+                    width={35}
+                    height={35}
+                  />
+                  <p className="text-2xl font-extrabold text-purple mt-6">
+                    {formatNumber(networkStatus.totalAddresses)}
+                  </p>
+                  <Label className="text-sm">Total Addresses</Label>
+                </div>
+              </div>
+            </TableCell>
+            <TableCell className="w-40 h-40 relative">
+              <div className="flex items-center justify-center bg-tablecell-detail rounded-xl h-full w-full">
+                <div className="text-center p-10 w-full">
+                  <Image
+                    src={Historical}
+                    alt="Historical"
+                    className="absolute top-0 left-0 ml-6 mt-6"
+                    width={35}
+                    height={35}
+                  />
+                  <p className="text-2xl font-extrabold text-purple">
+                    {networkStatus.avgBlockInterval}
+                  </p>
+                  <Label className="text-sm">AVG Block Intervals</Label>
+                </div>
+              </div>
+            </TableCell>
+            <TableCell className="w-40 h-40 relative">
+              <div className="flex items-center justify-center bg-tablecell-detail rounded-xl h-full w-full">
+                <div className="text-center p-10 w-full">
+                  <Image
+                    src={Pay}
+                    alt="Pay"
+                    className="absolute top-0 left-0 ml-6 mt-6"
+                    width={35}
+                    height={35}
+                  />
+                  <p className="text-2xl font-extrabold text-purple">
+                    {networkStatus.lastGasFee}
+                  </p>
+                  <Label className="text-sm">Last Gas Fee</Label>
+                </div>
+              </div>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </>
   )
 }
