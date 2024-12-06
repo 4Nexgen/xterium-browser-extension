83814a882db4d737d55d101ace2c488acb8546b3
butchcanz <butch.canada1994@gmail.com>
2024-12-06 13:06:28 +0800

    update token model

--- src/models/token.model.ts
+++ src/models/token.model.ts
@@ -1,9 +1,8 @@
 export class Token {
-    logo: string;
-    category: "Native" | "Asset";
-    network: string;
-    network_id: number;
-    symbol: string;
-    description: string;
-  }
-  
+  type: "Native" | "Asset"
+  network: string
+  network_id: number
+  symbol: string
+  description: string
+  image_url: string
+}
