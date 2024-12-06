7d30f7ca58060ede0bb12bf8d5fd982a51ac684f
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2024-12-06 10:31:08 +0800

    password and login services

---
+++ src/services/encryption.service.ts
@@ -0,0 +1,9 @@
+export class EncryptionService {
+    encryptPassword(decryptedPassword: string): string {
+        return "return the encrypted password here..."
+    }
+
+    decryptPassword(encryptedPassword: string): string {
+        return "return the decrypted password here..."
+    }
+}
