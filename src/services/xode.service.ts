e9c8625e9173a67fd43afb2597ffd9d09526c22e
butchcanz <butch.canada1994@gmail.com>
2024-12-13 15:42:41 +0800

    update xode service

--- src/services/xode.service.ts
+++ src/services/xode.service.ts
@@ -2,29 +2,33 @@ import { ApiPromise, WsProvider } from "@polkadot/api"
 
 export class XodeService {
   private api: ApiPromise | null = null
-  private isConnected: boolean = false
+  private readonly wsUrl = "wss://rpcnodea01.xode.net/n7yoxCmcIrCF6VziCcDmYTwL8R03a/rpc"
 
-  async connectToApi(wsUrl: string): Promise<ApiPromise> {
-    if (!this.api) {
-      const wsProvider = new WsProvider(wsUrl)
-      this.api = await ApiPromise.create({ provider: wsProvider })
-    }
-    return this.api
+  async connect(): Promise<ApiPromise | any> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (!this.api) {
+          const wsProvider = new WsProvider(this.wsUrl)
+          this.api = await ApiPromise.create({ provider: wsProvider })
+        }
+
+        resolve(this.api)
+      } catch (error) {
+        reject("Failed to connect to RPC")
+      }
+    })
   }
 
   async getTotalBlocks(): Promise<number | any> {
     return new Promise(async (resolve, reject) => {
       try {
-        const api = await this.connectToApi(
-          "wss://rpcnodea01.xode.net/n7yoxCmcIrCF6VziCcDmYTwL8R03a/rpc"
-        )
-
+        const api = await this.connect()
         const latestBlockHash = await api.rpc.chain.getBlock()
         const totalBlocks = latestBlockHash.block.header.number.toNumber()
 
         resolve(totalBlocks)
       } catch (error) {
-        reject(`Error getting total blocks: ${error.message}`)
+        reject(error)
       }
     })
   }
@@ -32,16 +36,13 @@ export class XodeService {
   async getTotalAddresses(): Promise<number | any> {
     return new Promise(async (resolve, reject) => {
       try {
-        const api = await this.connectToApi(
-          "wss://rpcnodea01.xode.net/n7yoxCmcIrCF6VziCcDmYTwL8R03a/rpc"
-        )
-
+        const api = await this.connect()
         const walletKeys = await api.query.system.account.keys()
         const totalAddresses = walletKeys.length
 
         resolve(totalAddresses)
       } catch (error) {
-        reject(`Error getting total addresses: ${error.message}`)
+        reject(error)
       }
     })
   }
