6200d5eef89134ae487a83b0fa51ce6559b34f0c
JM Mac <johnmarkmancol@gmail.com>
2024-12-13 17:07:25 +0800

    style: theme modes on network status

--- src/pages/application/network-status/index.tsx
+++ src/pages/application/network-status/index.tsx
@@ -1,5 +1,4 @@
 import { Label } from "@/components/ui/label"
-import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table"
 import { XodeService } from "@/services/xode.service"
 import { Blocks, HandCoins, Hourglass, Wallet } from "lucide-react"
 import React, { useEffect, useState } from "react"
@@ -50,36 +49,38 @@ const IndexNetworkStatus = () => {
     <>
       <div className="flex flex-wrap">
         <div className="flex items-center justify-center h-full w-1/2 p-2">
-          <div className="text-center py-10 w-full relative border-2 border-border bg-muted/50 rounded-lg h-full">
+          <div className="text-center py-10 w-full relative border-2 border-primary dark:border-border dark:bg-muted/50 rounded-lg h-full">
             <Blocks className="absolute top-0 left-0 ml-6 mt-6 opacity-50" size="30" />
-            <p className="text-2xl font-extrabold mt-6">
+            <p className="text-2xl font-extrabold mt-6 text-primary dark:text-white">
               {formatNumber(networkStatus.totalBlocks)}
             </p>
             <Label className="text-sm opacity-50">Total Blocks</Label>
           </div>
         </div>
         <div className="flex items-center justify-center h-full w-1/2 p-2">
-          <div className="text-center py-10 w-full relative border-2 border-border bg-muted/50 rounded-lg h-full">
+          <div className="text-center py-10 w-full relative border-2 border-primary dark:border-border dark:bg-muted/50 rounded-lg h-full">
             <Wallet className="absolute top-0 left-0 ml-6 mt-6 opacity-50" size="30" />
-            <p className="text-2xl font-extrabold mt-6">
+            <p className="text-2xl font-extrabold mt-6 text-primary dark:text-white">
               {formatNumber(networkStatus.totalAddresses)}
             </p>
             <Label className="text-sm opacity-50">Total Addresses</Label>
           </div>
         </div>
         <div className="flex items-center justify-center h-full w-1/2 p-2">
-          <div className="text-center py-10 w-full relative border-2 border-border bg-muted/50 rounded-lg h-full">
+          <div className="text-center py-10 w-full relative border-2 border-primary dark:border-border dark:bg-muted/50 rounded-lg h-full">
             <Hourglass className="absolute top-0 left-0 ml-6 mt-6 opacity-50" size="30" />
-            <p className="text-2xl font-extrabold mt-6">
+            <p className="text-2xl font-extrabold mt-6 text-primary dark:text-white">
               {networkStatus.avgBlockInterval}
             </p>
             <Label className="text-sm opacity-50">AVG Block Intervals</Label>
           </div>
         </div>
         <div className="flex items-center justify-center h-full w-1/2 p-2">
-          <div className="text-center py-10 w-full relative border-2 border-border bg-muted/50 rounded-lg h-full">
+          <div className="text-center py-10 w-full relative border-2 border-primary dark:border-border dark:bg-muted/50 rounded-lg h-full">
             <HandCoins className="absolute top-0 left-0 ml-6 mt-6 opacity-50" size="30" />
-            <p className="text-2xl font-extrabold mt-6">{networkStatus.lastGasFee}</p>
+            <p className="text-2xl font-extrabold mt-6 text-primary dark:text-white">
+              {networkStatus.lastGasFee}
+            </p>
             <Label className="text-sm opacity-50">Last Gas Fee</Label>
           </div>
         </div>
