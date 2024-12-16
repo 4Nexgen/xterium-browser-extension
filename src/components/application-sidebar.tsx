09648129fb0e166fd915bc8027d2402d80940bf5
JM Mac <johnmarkmancol@gmail.com>
2024-12-16 16:30:06 +0800

    feat: display version number

--- src/components/application-sidebar.tsx
+++ src/components/application-sidebar.tsx
@@ -251,6 +251,7 @@ const ApplicationSidebar = ({ onSetCurrentPage, onSetIsLogout }) => {
               </DropdownMenu>
             </SidebarMenuItem>
           </SidebarMenu>
+          <p className="text-xs opacity-50 text-right px-2">Xterium v.0.2.0 (Beta)</p>
         </SidebarFooter>
       </Sidebar>
 
