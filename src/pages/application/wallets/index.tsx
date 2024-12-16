d8e478650a0b8e69afc66efd4c9e21a0bd3956f4
JM Mac <johnmarkmancol@gmail.com>
2024-12-16 14:12:58 +0800

    feat: empty state for wallet page

--- src/pages/application/wallets/index.tsx
+++ src/pages/application/wallets/index.tsx
@@ -13,7 +13,7 @@ import type { NetworkModel } from "@/models/network.model.js"
 import type { WalletModel } from "@/models/wallet.model.js"
 import { NetworkService } from "@/services/network.service.js"
 import { WalletService } from "@/services/wallet.service.js"
-import { Check, Copy, Download, Trash } from "lucide-react"
+import { Check, Copy, Download, Trash, Wallet } from "lucide-react"
 import React, { useEffect, useState } from "react"
 
 import IndexAddWallet from "./addWallet.tsx"
@@ -110,82 +110,97 @@ const IndexWallet = () => {
     <>
       <div className="py-4 flex flex-col justify-between h-full">
         <div className="flex-1">
-          <Card className="mb-3">
-            <Table>
-              <TableBody>
-                {wallets
-                  .filter(
-                    (wallet) =>
-                      wallet.address_type ===
-                      (selectedNetwork ? selectedNetwork.name : "")
-                  )
-                  .map((address, index) => (
-                    <TableRow key={index} className="hover-bg-custom">
-                      <TableCell className="px-4">
-                        <div className="mb-[2px]">
-                          <span className="text-lg font-bold">{address.name}</span>
-                        </div>
-                        <span style={{ color: "#657AA2" }}>
-                          {address.public_key.slice(0, 6)}
-                        </span>
-                        <span className="text-gray-500">...</span>
-                        <span style={{ color: "#657AA2" }}>
-                          {address.public_key.slice(-4)}
-                        </span>
-                      </TableCell>
-                      <TableCell className="w-[40px] justify-center text-center">
-                        <TooltipProvider>
-                          <Tooltip>
-                            <TooltipTrigger>
-                              <button
-                                onClick={() => copyWallet(address.public_key)}
-                                className="w-full h-full flex items-center justify-center text-white">
-                                <Copy />
-                              </button>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                              <p>Copy Address</p>
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      </TableCell>
-                      <TableCell className="w-[40px] justify-center text-center">
-                        <TooltipProvider>
-                          <Tooltip>
-                            <TooltipTrigger>
-                              <button
-                                onClick={() => exportWallet(address)}
-                                className="w-full h-full flex items-center justify-center text-white">
-                                <Download />
-                              </button>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                              <p>Export Wallet</p>
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      </TableCell>
-                      <TableCell className="w-[30px] justify-center text-center text-red-500 pr-4">
-                        <TooltipProvider>
-                          <Tooltip>
-                            <TooltipTrigger>
-                              <button
-                                onClick={() => deleteWallet(address)}
-                                className="w-full h-full flex items-center justify-center">
-                                <Trash />
-                              </button>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                              <p>Delete Wallet</p>
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-          </Card>
+          {wallets.filter(
+            (wallet) =>
+              wallet.address_type === (selectedNetwork ? selectedNetwork.name : "")
+          ).length ? (
+            <>
+              <Card className="mb-3">
+                <Table>
+                  <TableBody>
+                    {wallets
+                      .filter(
+                        (wallet) =>
+                          wallet.address_type ===
+                          (selectedNetwork ? selectedNetwork.name : "")
+                      )
+                      .map((address, index) => (
+                        <TableRow key={index} className="hover-bg-custom">
+                          <TableCell className="px-4">
+                            <div className="mb-[2px]">
+                              <span className="text-lg font-bold">{address.name}</span>
+                            </div>
+                            <span style={{ color: "#657AA2" }}>
+                              {address.public_key.slice(0, 6)}
+                            </span>
+                            <span className="text-gray-500">...</span>
+                            <span style={{ color: "#657AA2" }}>
+                              {address.public_key.slice(-4)}
+                            </span>
+                          </TableCell>
+                          <TableCell className="w-[40px] justify-center text-center">
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger>
+                                  <button
+                                    onClick={() => copyWallet(address.public_key)}
+                                    className="w-full h-full flex items-center justify-center text-white">
+                                    <Copy />
+                                  </button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                  <p>Copy Address</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+                          </TableCell>
+                          <TableCell className="w-[40px] justify-center text-center">
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger>
+                                  <button
+                                    onClick={() => exportWallet(address)}
+                                    className="w-full h-full flex items-center justify-center text-white">
+                                    <Download />
+                                  </button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                  <p>Export Wallet</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+                          </TableCell>
+                          <TableCell className="w-[30px] justify-center text-center text-red-500 pr-4">
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger>
+                                  <button
+                                    onClick={() => deleteWallet(address)}
+                                    className="w-full h-full flex items-center justify-center">
+                                    <Trash />
+                                  </button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                  <p>Delete Wallet</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                  </TableBody>
+                </Table>
+              </Card>
+            </>
+          ) : (
+            <div className="flex flex-col gap-4 items-center py-[100px]">
+              <Wallet className="size-20" />
+              <div className="text-center">
+                <h4 className="font-bold text-lg">No Wallet Found</h4>
+                <p className="opacity-50">Add new wallet by clicking the button below.</p>
+              </div>
+            </div>
+          )}
         </div>
 
         <Button variant="jelly" className="my-auto" onClick={addWallet}>
