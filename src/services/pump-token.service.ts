4aae61010df9588e9d31acafeb4b0f7b8d71fcd2
butchcanz <butch.canada1994@gmail.com>
2025-01-20 12:41:09 +0800

    update pump token service

--- src/services/pump-token.service.ts
+++ src/services/pump-token.service.ts
@@ -1,10 +1,40 @@
 import { PumpTokenModel } from "@/models/pump-token.model"
+import { ApiPromise, WsProvider } from "@polkadot/api"
 
 import pumpTokens from "../data/pump-token/pump-tokens.json"
 import { BalanceServices } from "./balance.service"
+import { NetworkService } from "./network.service"
 
 export class PumpTokenService {
+  private networkService = new NetworkService()
+  private api: ApiPromise | null = null
   private balanceService = new BalanceServices()
+
+  async connect(): Promise<ApiPromise | any> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.networkService
+          .getNetwork()
+          .then(async (data) => {
+            let wsUrl = data.rpc
+
+            if (!this.api) {
+              const wsProvider = new WsProvider(wsUrl)
+              const api = await ApiPromise.create({ provider: wsProvider })
+
+              this.api = api
+
+              resolve(this.api)
+            }
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      } catch (error) {
+        reject("Failed to connect to RPC")
+      }
+    })
+  }
 
   async createPumpToken(data: PumpTokenModel): Promise<string> {
     return new Promise(async (resolve, reject) => {
@@ -82,6 +112,65 @@ export class PumpTokenService {
         resolve(balances)
       } catch (error) {
         reject(`Error fetching wallet balances: ${error.message}`)
+      }
+    })
+  }
+
+  async getAssetDetails(assetId: string): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let api = this.api
+
+        if (!api) {
+          api = await this.connect()
+          console.log("Connected to API:", api.isConnected)
+        }
+
+        const assetIdNumber = Number(assetId)
+        if (isNaN(assetIdNumber) || assetIdNumber < 0 || assetIdNumber > 2 ** 32 - 1) {
+          reject(new Error("Invalid assetId. Must be a valid u32."))
+          return
+        }
+
+        const asset = await api.query.assets.asset(assetIdNumber)
+        const metadata = await api.query.assets.metadata(assetIdNumber)
+
+        if (!asset || asset.isEmpty) {
+          console.warn("Asset not found for ID:", assetIdNumber)
+          reject(new Error("Asset not found."))
+          return
+        }
+
+        const assetDetails = asset.toHuman() as { [key: string]: any }
+        const metadataDetails = metadata.toHuman() as { [key: string]: any }
+
+        if (!assetDetails) {
+          console.error("Invalid asset details format.")
+          reject(new Error("Invalid asset details format."))
+          return
+        }
+
+        console.log("Asset Details:", {
+          assetId: assetIdNumber,
+          details: assetDetails
+        })
+
+        console.log("Asset Details:", {
+          assetId: assetIdNumber,
+          details: metadataDetails
+        })
+
+        resolve({
+          assetId: assetIdNumber,
+          name: metadataDetails.name || "Unknown",
+          symbol: metadataDetails.symbol || "N/A",
+          owner: assetDetails.owner || "Unknown",
+          assetDetails: assetDetails,
+          metadataDetails
+        })
+      } catch (error) {
+        console.error("Error fetching asset details:", error)
+        reject(error)
       }
     })
   }
