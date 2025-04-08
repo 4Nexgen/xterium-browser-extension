888c1452f5e2b90185d8a741a8d4e0d1f635eaa2
JM Mac <johnmarkmancol@gmail.com>
2025-04-08 18:18:27 +0800

    style: support page

--- src/pages/application/imurai/index.tsx
+++ src/pages/application/imurai/index.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea } from "@/components/ui/scroll-area"
 import type { VAModel } from "@/models/imurai.model"
 import type { Message, userMessage } from "@/models/message.model"
 import {
@@ -5,6 +6,9 @@ import {
   ImuraiService,
   imuraiWSURL
 } from "@/services/imurai.service"
+import ImuraiAvatar from "data-base64:/assets/imurai/imurai-avatar.png"
+import imuraiChatAvatar from "data-base64:/assets/imurai/imurai-chat-avatar.png"
+import activeIndicator from "data-base64:/assets/imurai/online-indicator.png"
 import { BotMessageSquare, Loader2, SendHorizonal } from "lucide-react"
 import React, { useEffect, useRef, useState } from "react"
 
@@ -119,90 +123,85 @@ const IndexImUrAi = () => {
   }
 
   return (
-    <div className="pt-4 flex flex-col gap-4 h-[calc(100vh-80px)]">
+    <div className="pt-4 px-4 flex flex-col gap-4 h-[calc(100vh-60px)]">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <img src={ImuraiAvatar} className="size-24" alt="Imurai Avatar" />
+          <img
+            src={activeIndicator}
+            className="size-4 absolute -top-[8px] left-[calc(50%-8px)] translate-x-50"
+            alt="Active indicator"
+          />
+        </div>
+      </div>
       <div className="flex-1 overflow-auto">
-        {conversation.length ? (
-          <div>
-            <ul className="flex flex-col gap-2 px-[10px]">
-              {conversation.map((item, k) => {
-                return (
-                  <li
-                    key={`conv-item-${k}`}
-                    className={`flex gap-1 py-2 ${item.role == "user" ? "self-end max-w-80" : ""}`}>
-                    {item.role != "user" ? (
-                      <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center p-2">
-                        {virtualAssistant ? (
-                          <img src={virtualAssistant.photo_url} alt="" />
-                        ) : (
-                          <BotMessageSquare className="size-6" />
-                        )}
-                      </div>
-                    ) : (
-                      ""
-                    )}
-                    <p
-                      className={`flex-1 px-4 rounded-lg ${item.role == "user" ? "justify-end bg-primary py-2" : ""}`}>
-                      {item.content}
-                    </p>
+        <ScrollArea className="h-full border-2 rounded-lg border-muted bg-[#0A4DBA33]">
+          {conversation.length ? (
+            <div>
+              <ul className="flex flex-col gap-2 px-[10px]">
+                {conversation.map((item, k) => {
+                  return (
+                    <li
+                      key={`conv-item-${k}`}
+                      className={`flex gap-1 py-2 ${item.role == "user" ? "self-end max-w-80" : ""}`}>
+                      {item.role != "user" ? (
+                        <div className="w-10 h-10 bg-[#0A4DBA] rounded-full flex items-center justify-center p-2">
+                          {virtualAssistant ? (
+                            <img src={imuraiChatAvatar} alt="" />
+                          ) : (
+                            <BotMessageSquare className="size-6" />
+                          )}
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                      <p
+                        className={`flex-1 px-4 rounded-lg ${item.role == "user" ? "justify-end bg-primary py-2" : ""}`}>
+                        {item.content}
+                      </p>
+                    </li>
+                  )
+                })}
+                {messageComposition != "" ? (
+                  <li className="flex py-2 gap-1">
+                    <div className="w-10 h-10 bg-[#0A4DBA] rounded-full flex items-center justify-center p-2">
+                      {virtualAssistant ? (
+                        <img src={imuraiChatAvatar} alt="" />
+                      ) : (
+                        <BotMessageSquare className="size-6" />
+                      )}
+                    </div>
+                    <p className="flex-1 px-4 rounded-lg">{messageComposition}</p>
                   </li>
-                )
-              })}
-              {messageComposition != "" ? (
-                <li className="flex py-2 gap-1">
-                  <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center p-2">
-                    {virtualAssistant ? (
-                      <img src={virtualAssistant.photo_url} alt="" />
-                    ) : (
-                      <BotMessageSquare className="size-6" />
-                    )}
-                  </div>
-                  <p className="flex-1 px-4 rounded-lg">{messageComposition}</p>
-                </li>
-              ) : (
-                ""
-              )}
-
-              {thinking ? (
-                <li className="flex py-2 gap-1">
-                  <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center p-2">
-                    {virtualAssistant ? (
-                      <img src={virtualAssistant.photo_url} alt="" />
-                    ) : (
-                      <BotMessageSquare className="size-6" />
-                    )}
-                  </div>
-                  <div className="loader"></div>
-                </li>
-              ) : (
-                ""
-              )}
-            </ul>
-          </div>
-        ) : (
-          <div>
-            <div
-              className="h-[150px] bg-primary rounded-lg relative mb-[100px] dev-bg-image"
-              style={{
-                backgroundImage: virtualAssistant
-                  ? `url(${virtualAssistant.cover_photo_url})`
-                  : ""
-              }}>
-              <div className="absolute -bottom-[50px] left-[50%]  -translate-x-[50%] w-[100px] h-[100px] rounded-full bg-white border-primary flex items-center justify-center">
-                {virtualAssistant ? (
-                  <img src={virtualAssistant.photo_url} alt="" />
                 ) : (
-                  <BotMessageSquare className="block size-16 text-primary" />
+                  ""
                 )}
-              </div>
+
+                {thinking ? (
+                  <li className="flex py-2 gap-1">
+                    <div className="w-10 h-10 bg-[#0A4DBA] rounded-full flex items-center justify-center p-2">
+                      {virtualAssistant ? (
+                        <img src={imuraiChatAvatar} alt="" />
+                      ) : (
+                        <BotMessageSquare className="size-6" />
+                      )}
+                    </div>
+                    <div className="loader"></div>
+                  </li>
+                ) : (
+                  ""
+                )}
+              </ul>
             </div>
-            <div className="flex flex-col items-center w-full">
+          ) : (
+            <div className="flex flex-col items-center w-full h-full pt-20">
               <h3 className="text-lg">Hello, How can I help you?</h3>
               <p className="w-80 text-center mt-4 opacity-50">
                 {virtualAssistant?.description}
               </p>
             </div>
-          </div>
-        )}
+          )}
+        </ScrollArea>
       </div>
       <form className="h-[60px] py-[5px] flex items-center" onSubmit={sendChatMessage}>
         <div className="bg-white rounded-lg flex gap-2 overflow-hidden py-2 px-4 w-full">
