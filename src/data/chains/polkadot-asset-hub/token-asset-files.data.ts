6db492180a88a81787a7a0178d3e50b74d89675c
joveryn092101 <pagunsanjoveryn@gmail.com>
2025-04-14 15:58:45 +0800

    add DON token

--- src/data/chains/polkadot-asset-hub/token-asset-files.data.ts
+++ src/data/chains/polkadot-asset-hub/token-asset-files.data.ts
@@ -4,12 +4,14 @@ import DefaultLogo from "data-base64:/assets/tokens/default.png";
 
 import DOTLogo from "data-base64:/assets/tokens/polkadot-asset-hub/dot.png";
 import MPCLogo from "data-base64:/assets/tokens/polkadot-asset-hub/mpc.png";
+import DONLogo from "data-base64:/assets/tokens/polkadot-asset-hub/don.png";
 
 export class TokenAssetFiles extends ChainAssetFiles {
   getTokenLogo(imageName: string): string {
     switch (imageName) {
       case "DOT": return DOTLogo;
       case "MPC": return MPCLogo;
+      case "DON": return DONLogo;
       default: return DefaultLogo;
     }
   }
