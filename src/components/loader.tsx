99218c8bf444072fc5aff154aa870757cacdf7f7
JM Mac <johnmarkmancol@gmail.com>
2025-05-05 20:18:58 +0800

    fix loader params

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,11 +1,15 @@
 import LoadingIndicator from "data-base64:/assets/ios-loading.gif"
 
-export default function Loader(label: string = null) {
+interface LoaderParams {
+  label?: string
+}
+
+export default function Loader({ label }: LoaderParams) {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen z-10">
       <div className="max-w-[250px] max-h-[150px] flex items-center justify-center rounded-lg bg-[#000000cc] left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] fixed flex flex-col items-center justify-center gap-1 py-4 px-8">
         <img src={LoadingIndicator} className="size-[40px]" />
-        {label ? <span>Loading...</span> : <span>{label}</span>}
+        {!label ? <span>Loading...</span> : <span>{label}</span>}
       </div>
     </div>
   )
