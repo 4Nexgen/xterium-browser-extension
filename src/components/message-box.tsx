7ffbda35cca5c140f1fac53179ae44ae52743a02
JM Mac <johnmarkmancol@gmail.com>
2025-05-05 20:10:28 +0800

    feat: loader label

--- src/components/message-box.tsx
+++ src/components/message-box.tsx
@@ -1,6 +1,6 @@
 import LoadingIndicator from "data-base64:/assets/ios-loading.gif"
 
-export default function Loader(label: string = null) {
+export default function MessageBox(label: string = null) {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen z-10">
       <div className="max-w-[250px] max-h-[150px] flex items-center justify-center rounded-lg bg-[#000000cc] left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] fixed flex flex-col items-center justify-center gap-1 py-4 px-8">
